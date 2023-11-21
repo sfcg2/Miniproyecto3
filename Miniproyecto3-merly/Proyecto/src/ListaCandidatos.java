27b90a824206511bfca62a211586cdef3142a39a
Mvc2004 <merly.velasquez@correounivalle.edu.co>
2023-11-21 16:52:36 -0500

    baje el proyecto

---
+++ Miniproyecto3-merly/Proyecto/src/ListaCandidatos.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ListaCandidatos {
+
+    public void listaCandidatos(){     
+        
+        ArrayList<Candidato> listaC = Insertar.getInscritos();   
+
+        for(int i=0; i<listaC.size();i++){
+            Interfazz.getArea().append("\n\nCANDIDATO # " + (i + 1) + "\n"+ listaC.get(i).toString());
+            Interfazz.getArea().append("\n-----------------------------------------------------");
+        }
+    }
+    
+}

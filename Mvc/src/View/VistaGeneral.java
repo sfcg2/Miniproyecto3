5b6f9a39921a93a3e1377f87ea55a2a4bb744073
sfcg2 <castillo.sofia@correounivalle.edu.co>
2023-11-21 16:56:46 -0500

    mvc carpetas+

---
+++ Mvc/src/View/VistaGeneral.java
@@ -0,0 +1,12 @@
+package View;
+
+public interface VistaGeneral {
+    
+    public String getNombre();
+    public String getCedula();
+    public String getCiudad();
+    public String getPartido_P();
+    public String getIdeologia();
+    public String getPromesas();
+
+}
